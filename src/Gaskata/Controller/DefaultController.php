4189cc7f7e9886bf807320dfe8bdabcceda5caea
Thomas Lundquist <github@bisonlab.no>
2015-01-23 10:46:05 +0100

    I wanted a pure ping function. No auth, nothing, just ACK. Nice for the first API test.

--- src/Gaskata/Controller/DefaultController.php
+++ src/Gaskata/Controller/DefaultController.php
@@ -2,16 +2,28 @@
 
 namespace Gaskata\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BisonLab\CommonBundle\Controller\CommonController as CommonController;
 
-class DefaultController extends Controller
+class DefaultController extends CommonController
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/ping", name="ping")
+     */
+    public function pingAction(Request $request)
+    {
+        return $this->returnRestData($request, "ACK");
     }
 }
